1d5d0228a254904b8161359bbb704c4a54962d74
arcpp.zju <arcpp.zju@9d0d14c2-b9c1-488d-2cc0-27dc4913c159>
2010-06-10 08:57:46 +0000

    set svn ignore

---
+++ create-index.sql
@@ -0,0 +1,57 @@
+CREATE INDEX index_0_0 ON py_phrase_0(s0, y0);
+CREATE INDEX index_0_1 ON py_phrase_0(phrase);
+
+CREATE INDEX index_1_0 ON py_phrase_1(s0, y0, s1, y1);
+CREATE INDEX index_1_1 ON py_phrase_1(s0, s1, y1);
+CREATE INDEX index_1_2 ON py_phrase_1(s0, y0, s1);
+CREATE INDEX index_1_3 ON py_phrase_1(phrase);
+
+CREATE INDEX index_2_0 ON py_phrase_2(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_2_1 ON py_phrase_2(s0, s1, s2, y2);
+CREATE INDEX index_2_2 ON py_phrase_2(s0, y0, s1, s2);
+CREATE INDEX index_2_3 ON py_phrase_2(s0, s1, y1, s2);
+CREATE INDEX index_2_4 ON py_phrase_2(phrase);
+
+CREATE INDEX index_3_0 ON py_phrase_3(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_3_1 ON py_phrase_3(s0, s1, s2, y2);
+CREATE INDEX index_3_2 ON py_phrase_3(s0, y0, s1, s2);
+CREATE INDEX index_3_3 ON py_phrase_3(s0, s1, y1, s2);
+CREATE INDEX index_3_4 ON py_phrase_3(phrase);
+
+CREATE INDEX index_4_0 ON py_phrase_4(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_4_1 ON py_phrase_4(s0, s1, s2, s3, s4);
+CREATE INDEX index_4_2 ON py_phrase_4(phrase);
+
+CREATE INDEX index_5_0 ON py_phrase_5(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_5_1 ON py_phrase_5(s0, s1, s2, s3, s4, s5);
+CREATE INDEX index_5_2 ON py_phrase_5(phrase);
+
+CREATE INDEX index_6_0 ON py_phrase_6(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_6_1 ON py_phrase_6(s0, s1, s2, s3, s4, s5, s6);
+
+CREATE INDEX index_7_0 ON py_phrase_7(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_7_1 ON py_phrase_7(s0, s1, s2, s3, s4, s5, s6, s7);
+
+CREATE INDEX index_8_0 ON py_phrase_8(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_8_1 ON py_phrase_8(s0, s1, s2, s3, s4, s5, s6, s7, s8);
+
+CREATE INDEX index_9_0 ON py_phrase_9(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_9_1 ON py_phrase_9(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9);
+
+CREATE INDEX index_10_0 ON py_phrase_10(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_10_1 ON py_phrase_10(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10);
+
+CREATE INDEX index_11_0 ON py_phrase_11(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_11_1 ON py_phrase_11(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11);
+
+CREATE INDEX index_12_0 ON py_phrase_12(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_12_1 ON py_phrase_12(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12);
+
+CREATE INDEX index_13_0 ON py_phrase_13(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_13_1 ON py_phrase_13(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13);
+
+CREATE INDEX index_14_0 ON py_phrase_14(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_14_1 ON py_phrase_14(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14);
+
+CREATE INDEX index_15_0 ON py_phrase_15(s0, y0, s1, y1, s2, y2);
+CREATE INDEX index_15_1 ON  py_phrase_15(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15);
