08c66e9cca59b2ed212fc3e148fc7f16dc6a3980
arcpp.zju <arcpp.zju@9d0d14c2-b9c1-488d-2cc0-27dc4913c159>
2010-06-28 04:07:40 +0000

    increase buffer size, a little faster importing scel

--- trunk/src/scel-import-selector.py
+++ trunk/src/scel-import-selector.py
@@ -10,7 +10,7 @@ def main():
     filter = gtk.FileFilter()
     filter.set_name("Scel Files")
     filter.add_pattern("*.scel")
-    chooser = gtk.FileChooserDialog('导入 .scel 词库', None, action = gtk.FILE_CHOOSER_ACTION_OPEN, buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+    chooser = gtk.FileChooserDialog('导入一个或多个 scel 词库', None, action = gtk.FILE_CHOOSER_ACTION_OPEN, buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
     chooser.set_default_response(gtk.RESPONSE_OK)
     chooser.set_select_multiple(True)
     chooser.set_action(gtk.FILE_CHOOSER_ACTION_OPEN)
